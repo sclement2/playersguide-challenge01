d23107068a0e63bcb6a4740ba61b8d4ec80bb2d1
sclement2 <sclement2@gmail.com>
2024-02-24 04:46:10 +0000

    Add egg challenges

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Runtime.Intrinsics.X86;
-using System.Threading;
-
+using System.Collections.Generic;
+ 
 namespace dotnetcore
 {
     class Program
@@ -10,7 +9,18 @@ namespace dotnetcore
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
+            //ChallengeFarmer();
+            ChallengeDuckbear();
+            DuckbearMore();
+        }
 
+        static float PythagCalc(float height, float length)
+        {
+            return length * height / 2.0f;
+        }
+
+        static void ChallengeFarmer()
+        {
             bool isValid = true;
             while (isValid)
             {
@@ -51,9 +61,44 @@ namespace dotnetcore
             }
         }
 
-        static float PythagCalc(float height, float length)
+        static void ChallengeDuckbear()
         {
-            return length * height / 2.0f;
+            Console.WriteLine("How many eggs were gathered today?");
+            int eggCount = int.Parse(Console.ReadLine());
+            int remainingEggs = eggCount % 4;
+            if (remainingEggs == 0)
+            {
+                Console.WriteLine($"The number of eggs each sister receives: {eggCount / 4}.\nThe duckbear does not receive any eggs. Sorry duckbear.");
+            }
+            else
+            {
+                Console.WriteLine($"The number of eggs each sister receives: {(eggCount - remainingEggs) / 4}.\nThe number of eggs the duckbear receives: {remainingEggs}");
+            }
+        }
+
+        static void DuckbearMore()
+        {
+            int count = 0;
+            List<int> eggCounts = [];
+            int remainingEggs;
+            int eggCount = 1;
+
+            while (count < 3)
+            {
+                remainingEggs = eggCount % 4;
+                if (remainingEggs > (eggCount - remainingEggs) / 4)
+                {
+                    eggCounts.Add(eggCount);
+                    count++;
+                }
+                eggCount++;
+
+            }
+
+            foreach (var item in eggCounts)
+            {
+                Console.WriteLine($"Egg count where the Duckbear receives more than the sisters: {item}");
+            }
         }
     }
 }
